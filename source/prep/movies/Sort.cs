13a49865c6ae643347d4d9c34707b74632844283
Nathan Magnus <nathanm@iqmetrix.com>
2017-03-21 13:17:04 -0600

    Pushing new changes

--- source/prep/movies/Sort.cs
+++ source/prep/movies/Sort.cs
@@ -4,29 +4,38 @@ using code.utility.matching;
 
 namespace code.prep.movies
 {
-  public class Sort<Item>
-  {
-    public static ICompareTwoItems<Item> by<Property>(IGetTheValueOfAProperty<Item, Property> accessor) where Property : IComparable<Property>
-    {
-      return (a, b) => accessor(a).CompareTo(accessor(b));
-    }
+	public delegate int SortOrderType(int a);
 
-    public static ICompareTwoItems<Item> by<Property>(IGetTheValueOfAProperty<Item, Property> accessor, params Property[] sort_order)
-    {
-      return (a, b) => Array.IndexOf(sort_order, accessor(a)) - Array.IndexOf(sort_order, accessor(b));
-    }
-  }
+	public class SortOrder
+	{
+		public static SortOrderType descending = i => -i;
+		public static SortOrderType ascending = i => i;
+	}
 
-  public static class SortExtensions
-  {
-    public static ICompareTwoItems<Item> then_by<Item, Property>(this ICompareTwoItems<Item> previous_comparer, IGetTheValueOfAProperty<Item, Property> accessor) where Property : IComparable<Property>
-    {
-      return (a, b) =>
-      {
-        var previous_result = previous_comparer(a, b);
-        return previous_result == 0 ? Sort<Item>.by(accessor)(a, b) : previous_result;
-      };
-    }
 
-  }
+	public class Sort<Item>
+	{
+		public static ICompareTwoItems<Item> by<Property>(IGetTheValueOfAProperty<Item, Property> accessor, SortOrderType sortOrder) where Property : IComparable<Property>
+		{
+			return (a, b) => sortOrder(accessor(a).CompareTo(accessor(b)));
+		}
+
+		public static ICompareTwoItems<Item> by<Property>(IGetTheValueOfAProperty<Item, Property> accessor, params Property[] sort_order)
+		{
+			return (a, b) => Array.IndexOf(sort_order, accessor(a)) - Array.IndexOf(sort_order, accessor(b));
+		}
+	}
+
+	public static class SortExtensions
+	{
+		public static ICompareTwoItems<Item> then_by<Item, Property>(this ICompareTwoItems<Item> previous_comparer, IGetTheValueOfAProperty<Item, Property> accessor, SortOrderType sortOrder) where Property : IComparable<Property>
+		{
+			return (a, b) =>
+			{
+				var previous_result = previous_comparer(a, b);
+				return previous_result == 0 ? Sort<Item>.by(accessor, sortOrder)(a, b) : sortOrder(previous_result);
+			};
+		}
+
+	}
 }
